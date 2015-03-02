32f5268017a0dffb88a10b4967a548a8af81d8fb
b-kolodziej <b.kolodziej@yahoo.com>
2015-03-02 14:20:03 +0100

    First commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bartosz/Pulpit/ecommerce/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ ecommerce/products/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, Http404
+
+# Create your views here.
+from .models import Product, ProductImage
+
+
+def search(request):
+	try:
+		q = request.GET.get('q')
+	except:
+		q = None
+	if q:
+		products = Product.objects.filter(title__icontains=q)
+		template = 'products/results.html'
+		context = { 'query' : q, 'products' : products}
+	else:
+		template = 'products/home.html'
+		context = {}
+	return render(request, template, context)
+
+
+def home(request):
+	products = Product.objects.all()
+	template = 'products/home.html'
+	context = {"products" : products }
+	return render(request, template, context)
+
+
+def all(request):
+	products = Product.objects.all()
+	context = {'products' : products}
+	template = 'products/all.html'
+	return render(request, template, context)
+
+
+
+def single(request, slug):
+	try:
+		product = Product.objects.get(slug=slug)
+		images = ProductImage.objects.filter(product=product)
+		context = {'product' : product, 'images' : images}
+		template = 'products/single.html'
+		return render(request, template, context)
+	except:
+		raise Http404
